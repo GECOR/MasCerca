886fb447fc08c45f63670d4a37f38ff7054d4dd7
Mr. D <uiktiomasfeliz@gmail.com>
2017-01-30 13:35:06 +0100

    'changes'

--- src/app/appConfig.ts
+++ src/app/appConfig.ts
@@ -1,10 +1,11 @@
 //export var urlGecorApi = "http://localhost/MasCercaAPI1/api/";
 
 //export var urlGecorApi = "http://192.168.1.76/MasCercaAPI1/api/";//danioc
-//export var urlGecorApi = "http://213.97.71.157/MasCercaAPI/api/";//Damianpc
-export var urlGecorApi = "http://mv0144.aytomalaga.intranet/MasCercaAPI/api/";//ayuntamiento Málaga
+export var urlGecorApi = "http://213.97.71.157/MasCercaAPI/api/";//Damianpc
+//export var urlGecorApi = "http://mv0144.aytomalaga.intranet/MasCercaAPI/api/";//ayuntamiento Málaga
 //export var urlGecorApi = "http://192.168.1.147/MasCercaAPI1/api/";//josepc
 //export var urlGecorApi = "http://192.168.1.75/MasCercaAPI1/api/";//antonio
 
 //export var urlSocket      = 'http://52.40.216.64:8086';
-export var urlSocket        = "http://mcchat.gecorcloud.com:8086";
+//export var urlSocket        = "http://mcchat.gecorcloud.com:8086";
+export var urlSocket        = "http://mcchat.gecorcloud.com";//chatmc
